50180934cd516f52b925acdc1bccf3eecff45930
Austin Tambakopoulos <atamb073@uottawa.ca>
2020-12-07 17:22:26 -0500

    Added support for default root user environments

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const { exec } = require("child_process");
 
 const image_version = core.getInput("image_version");
 const port = core.getInput("port");
+const user = core.getInput("root");
+const pass = core.getInput("pwd");
 
-const command = `sudo docker run -d -p ${port}:${port} mongo:${image_version}`;
+
+if (user && pass) {
+  const command = `sudo docker run -d -p ${port}:${port} mongo:${image_version} --auth -e MONGO_INITDB_ROOT_USERNAME=${user} -e MONGO_INITDB_ROOT_PASSWORD=${pass}`;
+} else {
+  const command = `sudo docker run -d -p ${port}:${port} mongo:${image_version}`;
+}
 
 console.log("Executing the following command: ");
 console.log(command);
